ca33c5ec6fe21913818a41b9a41988c813539e3c
AdrianoEdu <adriaoeduardotrentin@hotmail.com>
2026-06-28 13:57:57 -0300

    implements accounts screen

--- app/web/view/home/bank/accounts/page.tsx
+++ app/web/view/home/bank/accounts/page.tsx
@@ -7,7 +7,9 @@
 
 import Table, { TableColumn } from "@/app/web/components/table/table";
 import { GetCurrentAccountDto } from "@/app/web/dto/current-accont.dto";
-import { useState } from "react";
+import { currentAccountService } from "@/app/web/services/currentAccountService/currentAcountService";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const getColumns = (): TableColumn<GetCurrentAccountDto>[] => {
   return [
@@ -18,16 +20,53 @@ const getColumns = (): TableColumn<GetCurrentAccountDto>[] => {
   ];
 };
 
+let countCurrentAccounts = 0;
+const takeCurrentAccounts = 20;
+let oldCurrentAccountList: GetCurrentAccountDto[] = [];
+
 export default function AccountScreen() {
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
   const [list, setList] = useState<GetCurrentAccountDto[]>();
+
+  useEffect(() => {
+    handleFindCurrentAccounts(currentPage);
+  }, []);
+
+  const handleFindCurrentAccounts = async (page: number) => {
+    const currentSkip = (page - 1) * takeCurrentAccounts;
+
+    const { count, currentAccount } = await currentAccountService.findAll({
+      all: true,
+      skip: currentSkip,
+      select: {
+        id: true,
+        bank: true,
+        balance: true,
+        createdAt: true,
+        accountNumber: true,
+      },
+      take: takeCurrentAccounts,
+      orderBy: { createdAt: "desc" },
+    });
+
+    setList(currentAccount);
+    oldCurrentAccountList = currentAccount;
+
+    countCurrentAccounts = count;
+  };
+
+  const navigateBankStatement = (row: GetCurrentAccountDto): void => {
+    router.push(`/web/view/home/bank/accounts/${row.id}/${row.accountNumber}`);
+  };
 
   return (
     <div>
       <Table
         rows={list}
-        onRowClick={() => {}}
         columns={getColumns()}
         title={"Conta Corrente"}
+        onRowClick={navigateBankStatement}
       />
     </div>
   );
